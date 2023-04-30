cc63584505fd73e157507fc5108a5cbaeaaab452
Robson Vilaça <robsonvilaca.lima@gmail.com>
2023-04-29 21:42:46 -0300

    Criando Serviços e validadores
    _Validador de CPF
    _Serviço para salvar aluno e incluir validadores

--- fluencyNow/src/com/br/fluencynow/controller/Cadastrar_controller.java
+++ fluencyNow/src/com/br/fluencynow/controller/Cadastrar_controller.java
@@ -2,6 +2,8 @@ package com.br.fluencynow.controller;
 
 import com.br.fluencynow.dao.AlunoDAO;
 import com.br.fluencynow.model.Aluno;
+import com.br.fluencynow.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,20 +15,28 @@ import java.util.Date;
 @Controller
 public class Cadastrar_controller {
 
-        @RequestMapping("/cadastrar")
-        public ModelAndView cadastrar() {
+    @RequestMapping("/cadastrar")
+    public ModelAndView cadastrar() {
 
-            return new ModelAndView("cadastrarAluno");
-        }
+        return new ModelAndView("cadastrarAluno");
+    }
 
-        @RequestMapping("/cadastrarAluno")
-        public String adicionaAluno(Aluno aluno, HttpSession session) throws SQLException {
-            if(new com.br.fluencynow.dao.AlunoDAO().saveStudent(aluno)) {
+    @RequestMapping("/cadastrarAluno")
+    public String adicionaAluno(Aluno aluno, HttpSession session) throws SQLException {
+        try{
+
+            //continuar daqui esta retornando apenas a exception
+            boolean salvarAluno = new com.br.fluencynow.service.AlunoService().SalvarAluno(aluno);
+            if(salvarAluno == true){
                 session.setAttribute("alunoCadastrado", aluno);
                 return "redirect:administrador";
             }
-            return "redirect:home";
+
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
         }
+        return "redirect:home";
+    }
 
     }
 
---
+++ fluencyNow/src/com/br/fluencynow/service/AlunoService.java
@@ -0,0 +1,22 @@
+package com.br.fluencynow.service;
+
+import com.br.fluencynow.dao.AlunoDAO;
+import com.br.fluencynow.model.Aluno;
+
+import java.sql.SQLException;
+
+public class AlunoService {
+
+    public boolean SalvarAluno(Aluno aluno) throws SQLException {
+
+        if(new com.br.fluencynow.validadores.ValidaCPF().validarCPF(aluno.getCpf()) == false){
+            throw new IllegalArgumentException("CPF Invalido!");
+        }
+        boolean criarAluno = new com.br.fluencynow.dao.AlunoDAO().saveStudent(aluno);
+
+
+
+                //new saveStudent(aluno);
+        return criarAluno;
+    }
+}
